d1da753936b3ca439c8ddf46fa4bfc141d9155ea
unknown <steimel65@gmail.com>
2023-03-13 00:25:09 +0900

    chore: remove dead code

--- part_1/unicafe/src/App.js
+++ part_1/unicafe/src/App.js
@@ -17,11 +17,6 @@ const Total = (props) => {
   return <p>Number of exercises {props.course.parts[0]['exercises'] + props.course.parts[1]['exercises'] + props.course.parts[2]['exercises']}</p>
 }
 
-const Part = (props) => {
-  return (
-  <p>{props.part} {props.exercise}</p>
-  )
-}
 
 const App = () => {
   const course = {
