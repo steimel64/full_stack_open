2cdf5d71aa07d1f536738e03285e79c95645208f
unknown <steimel65@gmail.com>
2023-05-08 00:45:48 +0900

    feat: complete exercise 1.11

--- part_1/unicafe_part2/src/App.js
+++ part_1/unicafe_part2/src/App.js
@@ -1,18 +1,51 @@
 import { useState } from 'react'
 
-
-const Statistics = (props) => {
-  const total = props.good + props.bad + props.neutral
-  
+// Button Handler
+const Button = ({handleClick, text}) => {
   return (
-    <div>
-      all {props.good + props.bad + props.neutral}
-      average {(props.good * 1 + props.bad * -1 + props.neutral*0) / total}
-      positive {props.good / total}
-    </div>
-  )
+    <button onClick={handleClick}>
+    {text}
+    </button>
+  );
+  }
+
+// StatisticLine Handler including table formatting
+const StatisticLine = ({text, value}) => {
+  return(
+    <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+    </tr>
+  );
 }
 
+// Statistics Calculator
+const Statistics = (props) => {
+  const total = props.good + props.bad + props.neutral
+  const average = ((props.good * 1) + (props.bad * -1) + (props.neutral * 0)) / total
+  const positive = props.good / total
+  const positive_pct = positive.toLocaleString(undefined, {style: "percent", 
+                                                           minimumFractionDigits: 2});
+
+  // Render if feedback is > 0                                                         
+  if (total > 0) { 
+    return (
+      <div>
+        <table>
+        <StatisticLine text="good" value={props.good}/>
+        <StatisticLine text="neutral" value={props.neutral}/>
+        <StatisticLine text="bad" value={props.bad}/>
+        <StatisticLine text="all" value={total}/>
+        <StatisticLine text="average" value={average}/>
+        <StatisticLine text="positive" value={positive_pct}/>
+        </table>
+      </div>
+    )
+    // No feedback given
+    } else {
+      return <div>No Feedback given</div>
+    }
+}
 
 
 const App = () => {
@@ -20,8 +53,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
 
+  // click handlers
   const handleGoodClick = () => {
     setGood(good + 1)
   }
@@ -34,19 +67,15 @@ const App = () => {
     setBad(bad + 1)
   }
 
-
-
+  // render the UI
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={handleGoodClick}>good</button>
-      <button onClick={handleNeutralClick}>neutral</button>
-      <button onClick={handleBadClick}>bad</button>
+      <Button handleClick={handleGoodClick} text = "good" />
+      <Button handleClick={handleNeutralClick} text = "neutral" />
+      <Button handleClick={handleBadClick} text = "bad" />
 
       <h2>statistics</h2>
-      good {good}
-      neutral {neutral}
-      bad {bad}
       <Statistics good={good} neutral={neutral} bad={bad}/>
 
     </div>
@@ -54,3 +83,5 @@ const App = () => {
 }
 
 export default App
+
+// Completed 1.11* on 5/8/2023
